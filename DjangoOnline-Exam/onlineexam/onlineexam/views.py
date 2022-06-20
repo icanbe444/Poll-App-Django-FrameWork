58bb4b050959c132934a6bfc58652a7374e5527e
icanbe444 <icanbe444@gmail.com>
2022-06-20 13:34:15 +0100

    DRF Quiz API

--- DjangoOnline-Exam/onlineexam/onlineexam/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from onlineexam.models import Exam
-
-
-def examonline(request):
-    results = Exam.objects.all()
-    return render(request, 'index.html', {'Exam': results})
